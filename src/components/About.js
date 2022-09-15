fba343d0412b181f3f3846ccccc122c184a23294
Nazmul haque <bpinazmul18@gmail.com>
2022-09-15 12:22:54 +0600

    Working on about page.

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,17 @@ const About = () => {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
+            Hi, I'm Nazmul.
             <br className="hidden lg:inline-block" />I love to build amazing
             apps.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            I'm a Front-end developer with more than two years of experience in
+            the software industry, specialized in ReactJS and VueJS. My ambition
+            is improving or writing scalable, secure & maintainable software.
+            I'm constantly looking for interesting and challenging projects to
+            upgrade my skills and share my experience. I can be of great help to
+            you in your business development and improvement.
           </p>
           <div className="flex justify-center">
             <a
