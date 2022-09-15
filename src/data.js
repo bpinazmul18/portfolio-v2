63d63d29ada5ebb42f7b3cf0225214aa2dc0bee9
Nazmul haque <bpinazmul18@gmail.com>
2022-09-15 11:49:48 +0600

    Project initialize.

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    title: 'Project Title',
+    subtitle: 'Frontend Stack',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-1.gif',
+    link: 'https://reactbootcamp.com',
+  },
+]
+
+export const skills = [
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Vue.js',
+  'Next.js',
+  'Node',
+  'Express.js',
+  'Bootstrap',
+  'TailwindCSS',
+  'Material UI',
+]
