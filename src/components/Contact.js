eb5af536f5b12154cace5cc3dea5addd0ec1d683
Nazmul haque <bpinazmul18@gmail.com>
2022-09-15 12:39:28 +0600

    Fixed netlify attribute issues.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,7 +64,7 @@ const Contact = () => {
           </div>
         </div>
         <form
-          netlify
+          netlify="true"
           name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
