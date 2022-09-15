28db277fef543fffb4849a7e4230846d30c9c9b0
Nazmul haque <bpinazmul18@gmail.com>
2022-09-15 12:14:12 +0600

    Working on navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+            Nazmul Haque
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
@@ -22,8 +22,9 @@ const Navbar = () => {
           </a>
         </nav>
         <a
-          href="#contact"
+          href="https://www.upwork.com/freelancers/~01b18bdb33d779bf10"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+          target="_blank"
         >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
