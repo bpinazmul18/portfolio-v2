845ba86fb7a6b0bdb1b089ecc8a777c8f6bd89b3
Nazmul Haque <bpinazmul18@gmail.com>
2022-09-19 21:59:07 +0600

    Working on project section data.

--- src/data.js
+++ src/data.js
@@ -3,8 +3,16 @@ export const projects = [
     title: 'Marketplace for RacewayX Games',
     subtitle: 'Frontend Stack',
     description:
-      'Collect Rex tokens, purchase and tune new cars, buy cars to your garage for your collection to start racing for fortune!',
-    image: './project-1.gif',
+      'Setup themeforest gigaland (React) template with strapi, Design customized, react-router refactoring, api call, react-redux, responsive design and more.',
+    image: './racewayx.gif',
+    link: 'https://digsea.netlify.app/',
+  },
+  {
+    title: 'HypeScout for Influencer Marketing Platform',
+    subtitle: 'Frontend Stack',
+    description:
+      'Convert figma to react typescipt lading page, using react, react-bootstrap, react-router, api call, axios, data visualization, form and responsive design.',
+    image: './hype-scout.gif',
     link: 'https://digsea.netlify.app/',
   },
 ]
