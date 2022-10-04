7117923278a77c9ba3c8da835141a1335ce5771f
Nazmul Haque <bpinazmul18@gmail.com>
2022-10-04 21:51:36 +0600

    Working on testimonial and contact section.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,7 @@ const Contact = () => {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: 'opacity(0.7)' }}
-            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116834.15093167813!2d90.34924182813288!3d23.780620653383682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1664898378844!5m2!1sen!2sbd"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
@@ -45,8 +45,8 @@ const Contact = () => {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Katabon. <br />
+                Dhaka, Bangladesh
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,12 +54,12 @@ const Contact = () => {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                bpinazmul18@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">+8801785362016</p>
             </div>
           </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -30,4 +30,20 @@ export const skills = [
   'Material UI',
 ]
 
-export const testimonials = []
+export const testimonials = [
+  {
+    id: 1,
+    quote:
+      "Nazmul is a true professional. Great to work with, fabulous communication with high level of commitment and dedication. He is everything you're looking for all-around.",
+    image: './akintayo.jpeg',
+    name: 'Akintayo Kolade',
+    company: 'RacewayX Games',
+  },
+  {
+    id: 2,
+    quote: 'Excellent as always',
+    image: './akintayo.jpeg',
+    name: 'Akintayo Kolade',
+    company: 'RacewayX Games',
+  },
+]
